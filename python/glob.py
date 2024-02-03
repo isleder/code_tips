1e86e348c5d05a5bdc66311e12f4d7585494d207
Istvan Sleder <istvan.sleder@flocksafety.com>
2024-02-03 08:46:33 -0500

    scandir

--- python/glob.py
+++ python/glob.py
@@ -6,3 +6,11 @@ from pathlib import Path
 
 for path in Path('src').rglob('*.c'):
     print(path.name)
+
+for i, path in enumerate(Path('src').rglob('*.png')):
+    print(path.name)
+
+import os
+
+for fo in os.scandir("path"):
+    fo.
